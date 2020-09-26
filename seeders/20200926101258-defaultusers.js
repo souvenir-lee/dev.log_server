07b1167f9265d779452f2314cbb1ad54a1e26714
parfumes <xxparfumes@gmail.com>
2020-09-26 23:00:12 +0900

    update for RDS test

--- seeders/20200926101258-defaultusers.js
+++ seeders/20200926101258-defaultusers.js
@@ -17,28 +17,28 @@ module.exports = {
       [
         {
           email: 'souvenir-lee@test.com',
-          password: 1111,
+          password: process.env.TEMP,
           username: 'souvenir-lee',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           email: 'sjkwan92@test.com',
-          password: 1111,
+          password: process.env.TEMP,
           username: 'sjkwan92',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           email: 'jhkim9513@test.com',
-          password: 1111,
+          password: process.env.TEMP,
           username: 'jhkim9513',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           email: 'parfumes@test.com',
-          password: 1111,
+          password: process.env.TEMP,
           username: 'parfumes',
           createdAt: new Date(),
           updatedAt: new Date(),
