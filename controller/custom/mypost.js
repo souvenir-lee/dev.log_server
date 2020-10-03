f44d90410b0f4481d0fe80c517b1481f6d5c9de0
parfumes <xxparfumes@gmail.com>
2020-10-03 22:26:38 +0900

    correct login & update custom/mypost

--- controller/custom/mypost.js
+++ controller/custom/mypost.js
@@ -1,20 +1,38 @@
 /* eslint-disable no-unused-vars */
 const { user } = require('../../models');
 const { post } = require('../../models');
+const { comment } = require('../../models');
+const Sequelize = require('sequelize');
 
 module.exports = {
   get: (req, res) => {
     const id = Number(req.params.id); // ========= client와 추가 조율 필요(custom, 내가 쓴 글)
     console.log(req.params);
+
     post
       .findAll({
         raw: true,
         where: {
           authorId: id,
         },
+        attributes: [
+          ['id', 'postId'],
+          'title',
+          ['authorId', 'userId'],
+          'message',
+        ],
+        include: [
+          {
+            model: comment,
+            // required: true,
+            attributes: [
+              ['id', 'commentId'],
+              ['message', 'comment'],
+              ['userId', 'commentUserId'],
+            ],
+          },
+        ],
       })
-      .then((result) => {
-        res.send(result);
-      });
+      .then((result) => res.send(result));
   },
 };
